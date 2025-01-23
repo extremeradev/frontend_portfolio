75bb53ebafcf21407a127434159bc44c9035eb15
Extremera <extremera10extremera@gmail.com>
2025-01-23 22:48:40 +0100

    aumentando margin en footer

--- src/components/FooterSection.vue
+++ src/components/FooterSection.vue
@@ -2,11 +2,17 @@
     <footer class="border border-t-[#8A6FBF] border-l-transparent border-r-transparent border-b-transparent">
         <div class="container p-12 block justify-between mb-0">
             <div class="text-[#2C2C2C] font-bold text-[13px] sm:text-[14px] md:text-[18px] lg:text-[20px]">
-            Frontend realizado con Vite + Vue.js y Tailwind CSS<hr class="border-t-2 border-[#2c2c2c]">
-            
-            Backend realizado con NodeJS y Express</div>
-            <p class="text-slate-600 text-[12px] sm:text-[14px] md:text-[18px] lg:text-[18px] float-right sm:justify-left">Todos los
-                derechos reservados.</p>
+                Frontend realizado con Vite + Vue.js y Tailwind CSS
+                <hr class="border-t-2 border-[#2c2c2c]">
+
+                Backend realizado con NodeJS y Express
+            </div>
+            <div class="mt-4">
+                <p
+                    class="text-slate-600 text-[12px] sm:text-[14px] md:text-[18px] lg:text-[18px] float-right sm:justify-left">
+                    Todos los
+                    derechos reservados.</p>
+            </div>
         </div>
     </footer>
 </template>
