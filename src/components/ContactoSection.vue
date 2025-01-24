d86744d9ae2ade3f7a098e5837409ffb6706133a
Extremera <extremera10extremera@gmail.com>
2025-01-24 22:08:34 +0100

    subiendo modificaciones de enviar correo

--- src/components/ContactoSection.vue
+++ src/components/ContactoSection.vue
@@ -1,38 +1,85 @@
 <template>
   <section id="contact">
-    <div class="px-4 xl:pl-16 mt-4 md:mt-0 text-left flex flex-col z-10 h-full" data-aos="flip-right">
+    <div
+      class="px-4 xl:pl-16 mt-4 md:mt-0 text-left flex flex-col z-10 h-full"
+      data-aos="flip-right"
+    >
       <div class="flex">
-        <h2 class="text-4xl font-bold text-[#2C2C2C] text-left mb-4">Contacto
-        </h2>
+        <h2 class="text-4xl font-bold text-[#2C2C2C] text-left mb-4">Contacto</h2>
         <div class="w-1/4 ml-2 relative bottom-1">
-          <img @click="redirectToLinkedin" src="https://img.icons8.com/ios-glyphs/60/2c2c2c/linkedin.png"
-            class="cursor-pointer hover:filter hover:brightness-200 transition-all" />
+          <img
+            @click="redirectToLinkedin"
+            src="https://img.icons8.com/ios-glyphs/60/2c2c2c/linkedin.png"
+            class="cursor-pointer hover:filter hover:brightness-200 transition-all"
+          />
         </div>
       </div>
-      <form class="w-full" id="contact-form" @submit.prevent="sendEmail">
-        <div class="w-full mb-4 max-w-[290px] md:max-w-[100px] lg:max-w-[500px] xl:max-w-[700px]">
-          <label for="name" class="block mb-2 text-[#2C2C2C] font-semibold">Empresa</label>
-          <input v-model="email.subject" type="text" id="name" class="w-full p-3 bg-gray-300 text-[#2C2C2C] rounded-md"
-            required />
+
+      <!-- Formulario de contacto -->
+      <form
+        class="w-full"
+        id="contact-form"
+        @submit.prevent="sendEmail"
+      >
+        <div
+          class="w-full mb-4 max-w-[290px] md:max-w-[100px] lg:max-w-[500px] xl:max-w-[700px]"
+        >
+          <label
+            for="name"
+            class="block mb-2 text-[#2C2C2C] font-semibold"
+            >Empresa</label
+          >
+          <input
+            v-model="email.subject"
+            type="text"
+            id="name"
+            class="w-full p-3 bg-gray-300 text-[#2C2C2C] rounded-md"
+            required
+          />
         </div>
-        <div class="w-full mb-4 max-w-[290px] md:max-w-[100px] lg:max-w-[500px] xl:max-w-[700px]">
-          <label for="message" class="block mb-2 text-[#2C2C2C] font-semibold">Mensaje</label>
-          <textarea v-model="email.text" id="message" rows="4" class="w-full p-3 bg-gray-300 text-[#2C2C2C] rounded-md"
-            required></textarea>
+        <div
+          class="w-full mb-4 max-w-[290px] md:max-w-[100px] lg:max-w-[500px] xl:max-w-[700px]"
+        >
+          <label
+            for="message"
+            class="block mb-2 text-[#2C2C2C] font-semibold"
+            >Mensaje</label
+          >
+          <textarea
+            v-model="email.text"
+            id="message"
+            rows="4"
+            class="w-full p-3 bg-gray-300 text-[#2C2C2C] rounded-md"
+            required
+          ></textarea>
         </div>
         <div class="w-4/4 flex">
-          <button type="submit"
-            class="w-full relative top-1.5 mb-4 max-w-[70px] sm:max-w-[100px] md:max-w-[100px] lg:max-w-[100px] xl:max-w-[120px] bg-[#7F6A9D] p-3 text-white font-semibold rounded-md hover:bg-primary-dark">Enviar</button>
-
+          <button
+            type="submit"
+            :disabled="isLoading"
+            class="w-full relative top-1.5 mb-4 max-w-[110px] sm:max-w-[130px] md:max-w-[100px] lg:max-w-[100px] xl:max-w-[120px] bg-[#7F6A9D] p-3 text-white font-semibold rounded-md hover:bg-primary-dark disabled:opacity-50"
+          >
+            {{ isLoading ? "Enviando..." : "Enviar" }}
+          </button>
         </div>
-
       </form>
+    </div>
+
+    <!-- Loading spinner con fondo gris en todo el body -->
+    <div
+      v-if="isLoading"
+      class="fixed inset-0 flex items-center justify-center bg-gray-700 bg-opacity-70 z-50"
+    >
+      <div
+        class="animate-spin w-12 h-12 border-4 border-white border-t-transparent rounded-full"
+      ></div>
     </div>
   </section>
 </template>
 
 <script>
 import axios from "axios";
+import Swal from "sweetalert2";
 
 export default {
   data() {
@@ -41,6 +88,7 @@ export default {
         subject: "",
         text: "",
       },
+      isLoading: false, // Estado para controlar el loading
     };
   },
   methods: {
@@ -48,19 +96,41 @@ export default {
       window.open("https://www.linkedin.com/in/alejandro-l-6a410b247/");
     },
     async sendEmail() {
+      this.isLoading = true; // Activar el spinner
       try {
-        const response = await axios.post("https://backend-portfolio-wprm.onrender.com/send-email", {
-          subject: this.email.subject,
-          text: this.email.text,
-          html: `<p>${this.email.text}</p>`, // Convertimos el texto a HTML
+        const response = await axios.post(
+          "https://backend-portfolio-wprm.onrender.com/send-email",
+          {
+            subject: this.email.subject,
+            text: this.email.text,
+            html: `<p>${this.email.text}</p>`, // Convertimos el texto a HTML
+          }
+        );
+        // Mostrar un mensaje estiloso de éxito
+        Swal.fire({
+          icon: "success",
+          title: "¡Correo enviado!",
+          text: `Tu mensaje fue enviado correctamente con el ID: ${response.data.messageId}`,
+          confirmButtonColor: "#7F6A9D", // Personaliza el color del botón
+        }).then( () => {
+          window.location.reload();
         });
-        alert("Correo enviado: " + response.data.messageId);
+        
       } catch (error) {
         console.error("Error enviando correo:", error);
-        alert("Hubo un error al enviar el correo.");
+        Swal.fire({
+          icon: "error",
+          title: "¡Error!",
+          text: "Hubo un problema al enviar el correo. Por favor, intenta nuevamente.",
+          confirmButtonColor: "#FF6B6B", // Personaliza el color del botón
+        }).then( () => {
+          window.location.reload();
+        });
+        
+      } finally {
+        this.isLoading = false; // Desactivar el spinner
       }
     },
   },
 };
-
 </script>
---
+++ src/components/Loading.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+      <!-- Contenido principal -->
+      <slot></slot>
+  
+      <!-- Overlay de loading -->
+      <div
+        v-if="loadingStore.isLoading"
+        class="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50"
+      >
+        <div class="animate-spin w-12 h-12 border-4 border-blue-500 border-t-transparent rounded-full"></div>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  import { useLoadingStore } from "@/stores/loading";
+  
+  export default {
+    setup() {
+      const loadingStore = useLoadingStore();
+  
+      return {
+        loadingStore,
+      };
+    },
+  };
+  </script>
